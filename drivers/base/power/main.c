88e53323b11c63ff17e7b17952016d0f70472d58
Rafael J. Wysocki <rjw@sisk.pl>
2010-12-16 00:51:08 +0100

    PM: Remove redundant checks from core device resume routines
    Since a separate list of devices is used to link devices that have
    completed each stage of suspend (or resume), it is not necessary to
    check dev->power.status in the core device resume routines any more.
    
    Change-Id: Ie54a0047d8fcd113e8155d5a666ace5763cea8ea
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -375,22 +375,18 @@ void dpm_resume_noirq(pm_message_t state)
 	transition_started = false;
 	while (!list_empty(&dpm_noirq_list)) {
 		struct device *dev = to_device(dpm_noirq_list.next);
+		int error;
 
 		get_device(dev);
-		if (dev->power.status > DPM_OFF) {
-			int error;
+		dev->power.status = DPM_OFF;
+		list_move_tail(&dev->power.entry, &dpm_suspended_list);
+		mutex_unlock(&dpm_list_mtx);
 
-			dev->power.status = DPM_OFF;
-			mutex_unlock(&dpm_list_mtx);
+		error = device_resume_noirq(dev, state);
+		if (error)
+			pm_dev_err(dev, state, " early", error);
 
-			error = device_resume_noirq(dev, state);
-
-			mutex_lock(&dpm_list_mtx);
-			if (error)
-				pm_dev_err(dev, state, " early", error);
-		}
-		if (!list_empty(&dev->power.entry))
-			list_move_tail(&dev->power.entry, &dpm_suspended_list);
+		mutex_lock(&dpm_list_mtx);
 		put_device(dev);
 	}
 	mutex_unlock(&dpm_list_mtx);
@@ -503,18 +499,17 @@ static void dpm_resume(pm_message_t state)
 	while (!list_empty(&dpm_suspended_list)) {
 		dev = to_device(dpm_suspended_list.next);
 		get_device(dev);
-		if (dev->power.status >= DPM_OFF) {
-			int error;
+		int error;
 
-			dev->power.status = DPM_RESUMING;
-			mutex_unlock(&dpm_list_mtx);
+		dev->power.status = DPM_RESUMING;
+		mutex_unlock(&dpm_list_mtx);
 
-			error = device_resume(dev, state);
+		error = device_resume(dev, state);
+		if (error)
+		    pm_dev_err(dev, state, "", error);
 
-			mutex_lock(&dpm_list_mtx);
-			if (error)
-				pm_dev_err(dev, state, "", error);
-		}
+		mutex_lock(&dpm_list_mtx);
+
 		if (!list_empty(&dev->power.entry))
 			list_move_tail(&dev->power.entry, &dpm_prepared_list);
 		put_device(dev);
@@ -567,17 +562,14 @@ static void dpm_complete(pm_message_t state)
 		struct device *dev = to_device(dpm_prepared_list.prev);
 
 		get_device(dev);
-		if (dev->power.status > DPM_ON) {
-			dev->power.status = DPM_ON;
-			mutex_unlock(&dpm_list_mtx);
+		dev->power.status = DPM_ON;
+		list_move(&dev->power.entry, &list);
+		mutex_unlock(&dpm_list_mtx);
 
-			device_complete(dev, state);
-			pm_runtime_put_sync(dev);
+		device_complete(dev, state);
+		pm_runtime_put_sync(dev);
 
-			mutex_lock(&dpm_list_mtx);
-		}
-		if (!list_empty(&dev->power.entry))
-			list_move(&dev->power.entry, &list);
+		mutex_lock(&dpm_list_mtx);
 		put_device(dev);
 	}
 	list_splice(&list, &dpm_list);
